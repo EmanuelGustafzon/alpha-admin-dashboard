29d3142dfd79550221f4b543e6f5e8707f4bbb78
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2025-04-02 22:29:42 +0200

    working on the project form and custumzing a plugin to add members

--- Presentation/Controllers/HomeController.cs
+++ Presentation/Controllers/HomeController.cs
@@ -2,14 +2,19 @@
 using Business.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
 
 namespace Presentation.Controllers;
 
 [Authorize]
-public class HomeController() : Controller
+public class HomeController(IMemberService memberService) : Controller
 {
+    private readonly IMemberService _memberService = memberService;
     public async Task<IActionResult> Index()
     {
-        return View();
+        var model = new ProjectViewModel();
+        var result = await _memberService.GetAllMembersAsync();
+        model.Members = result.Data;
+        return View(model);
     }
 }
---
+++ Presentation/Models/ProjectViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Business.Models;
+using Domain.Models;
+
+namespace Presentation.Models
+{
+    public class ProjectViewModel
+    {
+        public ProjectForm ProjectForm { get; set; } = new();
+
+        public IEnumerable<Member> Members { get; set; } = [];
+    }
+}
