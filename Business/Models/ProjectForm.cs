6d564ebaedfc558c2de2e4917346940cfa104a7e
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2025-04-04 21:13:36 +0200

    add quill js and changed some styling project form

--- Business/Models/ProjectForm.cs
+++ Business/Models/ProjectForm.cs
@@ -1,11 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
 
 namespace Business.Models
 {
     public class ProjectForm
     {
-
-        public string? Description { get; set; }
+        public IFormFile? Image { get; set; }
+        public string? ImageUrl { get; set; }
+        public string ProjectName { get; set; } = null!;
+        public string ClientName { get; set; } = null!;
+        public string Description { get; set; } = null!;
         public List<string> MemberIds { get; set; } = [];
+        public decimal Budget { get; set; }
     }
 }
